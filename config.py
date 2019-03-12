a4d0134aa048289fb3923dbd780c84070f3e4488
sudeshna <sudeshna@localhost.localdomain>
2019-03-12 07:11:50 +0530

    adding config file

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class config(object):
+	DEBUG = False
+	TESTING = False
+	CSRF_ENABLED = True
+	SECRET_KEY = 'this-really-needs-to-be-changed'
+
+class ProductionConfig(config):
+	DEBUG = False
+
+class StagingConfig(config):
+	DEVELOPMENT = True
+	DEBUG = True
+
+class DevelopmentConfig(config):
+	DEVELOPMENT = True
+	DEBUG = True
+
+class TestingConfig(config):
+	TESTING = True
